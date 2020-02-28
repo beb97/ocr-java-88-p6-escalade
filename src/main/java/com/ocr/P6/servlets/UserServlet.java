67779cc7a38ad809ce5ea9e52143a78d6e8e4530
unknown <beb97@hotmail.com>
2020-02-28 13:52:33 +0100

    mise en place servlet

---
+++ src/main/java/com/ocr/P6/servlets/UserServlet.java
@@ -0,0 +1,33 @@
+package com.ocr.P6.sevlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("UTF-8");
+
+        PrintWriter out = response.getWriter();
+        out.println("<!DOCTYPE html>");
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<meta charset=\"utf-8\" />");
+        out.println("<title>Test</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<p>Bonjour !</p>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
